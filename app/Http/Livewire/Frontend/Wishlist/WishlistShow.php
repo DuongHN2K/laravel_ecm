32c0bf886fc2924d897dff64e8ebb4f370c04575
DuongHN2K <nhatduonghoang59@gmail.com>
2023-05-04 10:06:25 +0700

    Moved WishlistShow into Livewire/Frontend/Wishlist folder

--- app/Http/Livewire/Frontend/Wishlist/WishlistShow.php
+++ app/Http/Livewire/Frontend/Wishlist/WishlistShow.php
@@ -20,7 +20,7 @@ class WishlistShow extends Component
     public function render()
     {
         $wishlist = Wishlist::where('user_id', auth()?->user()?->id)->get();
-        return view('livewire.frontend.wishlist-show', [
+        return view('livewire.frontend.wishlist.wishlist-show', [
             'wishlist' => $wishlist
         ]);
     }
