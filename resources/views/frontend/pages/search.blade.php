b7028fb5804b4a1eb34056fb85d52f25b7885421
DuongHN2K <nhatduonghoang59@gmail.com>
2023-05-16 15:39:45 +0700

    Finished product search function

---
+++ resources/views/frontend/pages/search.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.app')
+
+@section('title', 'Tìm kiếm sản phẩm')
+
+@section('content')
+<div class="py-5">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-10">
+                <h4>Kết quả tìm kiếm</h4>
+                <div class="underline mb-4"></div>
+            </div>
+            
+            @forelse ($searchProducts as $proditem)
+                <div class="col-md-10">
+                    <div class="product-card">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <div class="product-card-img">
+                                    <label class="stock bg-danger">Mới</label>
+                                    <img src="{{ asset('images/products/thumbnail/' . $proditem->thumbnail) }}" alt="thumbnail">
+                                </div>
+                            </div>
+                            
+                            <div class="col-md-9">
+                                <div class="product-card-body">
+                                    <p class="product-brand">{{ $proditem->brand->name }}</p>
+                                    <h5 class="product-name">
+                                        <a href="{{ url('/collections/' . $proditem->category->slug . '/' . $proditem->slug ) }}">
+                                            {{ $proditem->name }}
+                                        </a>
+                                    </h5>
+                                    <div>
+                                        <span class="selling-price">{{ $proditem->price }} VNĐ</span>
+                                        {{-- <span class="original-price">$799</span> --}}
+                                    </div>
+                                    
+                                    <p style="height: 45px; overflow: hidden">
+                                        <b>Mô tả: </b> {!! $proditem->description !!}
+                                    </p>
+                                    <a href="{{ url('/collections/' . $proditem->category->slug . '/' . $proditem->slug ) }}" class="btn btn-outline-primary">
+                                        Chi tiết
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @empty
+                <div class="col-md-10 p-2">
+                    <h4>Không tìm thấy sản phẩm nào</h4>
+                </div>
+            @endforelse
+
+            <div>
+                {{ $searchProducts->appends(request()->input())->links() }}
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
